e6729ac749cb50ee86101d6f03042e77982bf69c
Kumodatsu <dragonkumo@gmail.com>
2020-10-12 02:49:03 +0200

    Allow 123 from whispers

--- src/invite123.lua
+++ src/invite123.lua
@@ -1,15 +1,53 @@
 local addon_name, I123 = ...
 
+local keywords = {
+    "123"
+}
+
+local events = {
+    "CHAT_MSG_WHISPER",
+    "CHAT_MSG_GUILD"
+}
+
+local contains = function(xs, y)
+    for _, x in ipairs(xs) do
+        if x == y then return true end
+    end
+    return false
+end
+
+local is_keyword = function(word)
+    return contains(keywords, word)
+end
+
+local is_event = function(event)
+    return contains(events, event)
+end
+
+local _self = UnitName "player"
+
+local in_group = function()
+    return IsInGroup() or IsInRaid()
+end
+
+local invite = function(player)
+    if player == _self or not in_group() then return end
+    InviteUnit(player)
+end
+
 -- Event handling
 local frame_events = CreateFrame("FRAME", "CS_EventFrame")
 frame_events.OnEvent = function(self, event, arg1, arg2)
-    if event == "CHAT_MSG_GUILD" then
+    if is_event(event) then
         local msg    = arg1
         local player = arg2
-        if msg == "123" then
-            InviteUnit(player)
+        if is_keyword(msg) then
+            invite(player)
         end
     end
 end
 frame_events:SetScript("OnEvent", frame_events.OnEvent)
-frame_events:RegisterEvent "CHAT_MSG_GUILD"
+
+for _, event in ipairs(events) do
+    frame_events:RegisterEvent(event)
+end
